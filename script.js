0eb884b7331436d6747ec2ffd48f8a0f144d87e1
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-19 10:37:00 +0100

    Step 22

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ function cleanInputString(str) {
     const cleanStrArray = [];
 
     for (let i = 0; i < strArray.length; i++) {
-
+        if (!["+", "-", " "].includes(strArray[i])) {
+            cleanStrArray.push(strArray[i])
+        }
     }
 }
