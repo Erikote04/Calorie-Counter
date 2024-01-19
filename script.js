82f7f5ebfdd44ab856ad14fb8c3b90630c69efde
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-19 10:26:55 +0100

    Step 21

--- script.js
+++ script.js
@@ -12,4 +12,8 @@ let isError = false;
 function cleanInputString(str) {
     const strArray = str.split('');
     const cleanStrArray = [];
+
+    for (let i = 0; i < strArray.length; i++) {
+
+    }
 }
