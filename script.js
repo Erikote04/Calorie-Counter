272f645887700c5ecf8eee21269e6d662f6fb58c
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-19 11:02:41 +0100

    Step 42

--- script.js
+++ script.js
@@ -21,5 +21,5 @@ function isInvalidInput(str) {
 
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll();
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
 }
