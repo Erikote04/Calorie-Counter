13be014d1ff65eb248a7af6be7ddcb771eeed392
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-20 19:02:06 +0100

    Step 60

--- script.js
+++ script.js
@@ -36,6 +36,9 @@ function getCaloriesFromInputs(list) {
     for (let i = 0; i < list.length; i++) {
         const currVal = cleanInputString(list[i].value);
         const invalidInputMatch = isInvalidInput(currVal);
+        if (invalidInputMatch) {
+
+        }
     }
 }
 
