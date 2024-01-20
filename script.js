457e19c54c56869f1347ed5f1f7a1bad2baacce5
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-20 18:49:20 +0100

    Step 51

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function isInvalidInput(str) {
 
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
-    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
     const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
     <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
