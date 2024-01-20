d4f42283e19a108d18a98b705ee073a985140901
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-20 19:23:55 +0100

    Step 73

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ function calculateCalories(e) {
     const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
     const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 }
 
 function getCaloriesFromInputs(list) {
