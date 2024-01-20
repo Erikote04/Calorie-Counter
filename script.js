faa553afc18936e79a17b59c6c1deb43f73e2bbe
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-20 19:17:00 +0100

    Step 68

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ function calculateCalories(e) {
     isError = false;
 
     const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type=number]");
+    const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
 }
 
 function getCaloriesFromInputs(list) {
