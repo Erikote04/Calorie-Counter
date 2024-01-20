45a9ca74226dc898c3d69048d287f757489dd5ae
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-20 18:40:50 +0100

    Step 47

--- script.js
+++ script.js
@@ -24,5 +24,6 @@ function addEntry() {
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
     const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />`;
+    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>`;
 }
