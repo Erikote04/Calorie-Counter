350bd3a55920a9a2557e1d7776e0f586bc450975
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-20 18:42:44 +0100

    Step 48

--- script.js
+++ script.js
@@ -25,5 +25,6 @@ function addEntry() {
     const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
     <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
-    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>`;
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" id="${entryDropdown.value}-${entryNumber}-calories" min="0" placeholder="Calories" />`;
 }
