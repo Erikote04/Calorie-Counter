d3c5ecb956b15a51819f96763ba6faf4b8b61a53
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-21 18:24:15 +0100

    Step 85

--- script.js
+++ script.js
@@ -59,6 +59,8 @@ function calculateCalories(e) {
     <p>${budgetCalories} Calories Budgeted</p>
     <p>${consumedCalories} Calories Consumed</p>
     <p>${exerciseCalories} Calories Burned</p>`;
+
+    output.classList.remove("hide");
 }
 
 function getCaloriesFromInputs(list) {
