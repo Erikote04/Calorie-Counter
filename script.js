de8f941b2607c120d6908c1e4218199a425228a3
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-01-21 18:10:15 +0100

    Step 78

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ function calculateCalories(e) {
     const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
     const surplusOrDeficit = remainingCalories >= 0 ? "Surplus" : "Deficit";
+    output.innerHTML = ``;
 }
 
 function getCaloriesFromInputs(list) {
